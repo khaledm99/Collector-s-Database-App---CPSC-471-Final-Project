463b4c74f81fb314b3c4e1d5d3974f56f66ff7de
khaledm99 <67652855+khaledm99@users.noreply.github.com>
2022-11-28 20:47:07 -0700

    Update index.php
    fixed password

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $connection = mysqli_connect("localhost","root","","main");
+    $connection = mysqli_connect("localhost","root","password","main");
     if(!$connection) {
         exit("there was an error".mysqli_connect_errno());
     } else {
