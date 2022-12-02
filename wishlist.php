5907a7ef24801d949c81851accddcfd1409ab0ae
khaledm99 <67652855+khaledm99@users.noreply.github.com>
2022-12-02 14:25:43 -0700

    Implemented Basic Wishlist display
    For wishlist page, simply iterates over each item in wishlist and prints its name.
    Also implemented SQL-injection prevention for all pages, and empty input field validation on register

--- wishlist.php
+++ wishlist.php
@@ -12,14 +12,32 @@
         header("Location: login.php?=loggedout");
     }
 
-    $query = "SELECT Name FROM ITEM, TITLE  WHERE TITLE.ITEM_ID = ITEM.ITEM_ID AND ITEM.Wishlist_name = '$username' UNION select Name FROM ITEM, CONSOLE WHERE ITEM.ITEM_ID = CONSOLE.ITEM_ID AND ITEM.Wishlist_name = '$username'";
-    $result = mysqli_query($connection, $query);
-    if (!$result) {
-        echo("sup_coll_err");
-    } else {
-        while ($o = mysqli_fetch_object($result)) {
-            printf("%s ", $o->Name);
-            printf("\n");
+    $query = "SELECT Name FROM ITEM, TITLE  WHERE TITLE.ITEM_ID = ITEM.ITEM_ID AND ITEM.Wishlist_name = ? 
+              UNION select Name FROM ITEM, CONSOLE WHERE ITEM.ITEM_ID = CONSOLE.ITEM_ID AND ITEM.Wishlist_name = ?
+              UNION select Name FROM ITEM, CONTROLLER WHERE ITEM.ITEM_ID = CONTROLLER.ITEM_ID AND ITEM.Wishlist_name = ?
+              UNION select Name FROM ITEM, STORAGE_DEVICE WHERE ITEM.ITEM_ID = STORAGE_DEVICE.ITEM_ID AND ITEM.Wishlist_name = ?
+              UNION select Name FROM ITEM, MISC_PERIPHERAL WHERE ITEM.ITEM_ID = MISC_PERIPHERAL.ITEM_ID AND ITEM.Wishlist_name = ?
+              UNION select Name FROM ITEM, SUBSCRIPTION WHERE ITEM.ITEM_ID = SUBSCRIPTION.ITEM_ID AND ITEM.Wishlist_name = ?";
+    if($prepared_query = mysqli_prepare($connection, $query)){
+        mysqli_stmt_bind_param($prepared_query, 'ssssss', $username, $username, $username, $username, $username, $username);
+        if(mysqli_stmt_execute($prepared_query)){
+            $result = mysqli_stmt_get_result($prepared_query);
+            while ($o = mysqli_fetch_object($result)) {
+                printf("%s ", $o->Name);
+                echo ('</br>');
+            }
+        } else {
+            echo("Error executing SQL");
         }
     }
+    
+    
+    
 ?>
+</br>
+<form action="dashboard.php">
+    <input type = "submit" name = "Dashboard" value = "Return to Dashboard">
+</form>
+<form action="wishlist.php" method="post">
+    <input type = "submit" name = "Logout" value = "Logout">
+</form>
