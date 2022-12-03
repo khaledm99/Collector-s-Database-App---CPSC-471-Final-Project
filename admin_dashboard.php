4c0c7eae31961d15ffe1dbd56d90fe97e7a8151d
khaledm99 <67652855+khaledm99@users.noreply.github.com>
2022-12-03 04:04:12 -0700

    Updated login for admin, added admin dashboard
    Added a check against admin usernames to login, if logging in with admin username redirects to admin dashboard. added basic dashboard with a generate system report button

---
+++ admin_dashboard.php
@@ -0,0 +1,47 @@
+<?php
+    session_start();
+    $connection = mysqli_connect("localhost","root","password","main");
+    if(!$connection) {
+        exit("there was an error".mysqli_connect_errno());
+    } 
+    $username = $_SESSION['username'] ;
+    echo "Welcome, administrator $username "  ;
+    
+    if (isset($_POST['Logout'])) {
+        session_unset();
+        session_destroy();
+        header("Location: login.php?=loggedout");
+    }
+    if (isset($_POST['gen_report'])) {
+       
+        $time = date("Y-m-d H:i:s");
+        $query = "INSERT INTO SYSTEM_REPORT (Timestamp) VALUES (?)";
+        if($prepared_query = mysqli_prepare($connection, $query)){
+            mysqli_stmt_bind_param($prepared_query, 's', $time);
+            mysqli_stmt_execute($prepared_query);
+            mysqli_stmt_store_result($prepared_query);
+            
+            $result = mysqli_stmt_affected_rows($prepared_query);
+        }
+        $query = "INSERT INTO GENERATES_SYS_REPORT (Admin_username, Report_timestamp, Super_collection_name) VALUES (?, ?, 'khaledm99''s collection')";
+        
+        if($prepared_query = mysqli_prepare($connection, $query)){
+            mysqli_stmt_bind_param($prepared_query, 'ss', $username, $time);
+            mysqli_stmt_execute($prepared_query);
+            mysqli_stmt_store_result($prepared_query);
+            
+            $result = mysqli_stmt_affected_rows($prepared_query);
+        }
+        echo("</br> System Report Generated at $time!");
+        
+    }
+?>
+
+<form action="admin_dashboard.php" method="post">
+    <input type = "submit" name = "gen_report" value = "Generate System Report">
+</form>
+
+
+<form action="admin_dashboard.php" method="post">
+    <input type = "submit" name = "Logout" value = "Logout">
+</form>
